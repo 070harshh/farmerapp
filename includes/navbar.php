e5ab83d7c8a0b90a1f34a1dcb041c7646b5ea9b0
070harshh <106022620+070harshh@users.noreply.github.com>
2024-07-26 16:08:38 +0530

    Initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,47 @@
+<header class="header">
+        <div class="left-section">
+            <a href="index.php">
+            <img class="logo" src="photos/logo_fin.png"></a>
+            <input class="search-box" type="text" placeholder="Search">
+
+        </div>
+        <div class="middle-section">
+            <div class="top-icon">
+                <img class="home" src="icons/home.svg">
+                <a href="index.php">Home</a>
+
+            </div>
+            <div class="top-icon">
+                <img style="height: 20px;" class="home" src="icons/research-left-svgrepo-com.svg">
+                <label><a href="research.php">Research</a></label>
+            </div>
+            <div class="top-icon">
+                <img style="height: 25px;" class="home" src="icons/scheme-svgrepo-com.svg">
+                <label><a href="schemes.php">Govt. Schemes</a></label>
+            </div>
+            <div class="top-icon">
+                <img class="home" src="icons/reward-symbol-in-a-circle-svgrepo-com.svg">
+                <label><a href="rewards.php">Rewards</a></label>
+            </div>
+            <div class="notification-container">
+                <img class="notification" src="icons/notification-on-svgrepo-com.svg">
+                <label>Notifications</label>
+                <div style="font-size: 14px;" class="notification-alert"></div>
+            </div>
+            <div class="me-icon">
+                <img class="dp" src="icons/profile-1336-svgrepo-com.svg">
+                <label style="opacity: 0.6;font-family: bold;"><a href="profile.php">Me</a><i class="fa fa-sort-desc"></i></label>
+
+            </div>
+        </div>
+        <div class="right-section">
+            <div class="top-icon">
+                <img style="height: 20px;" class="home" src="icons/square-4-grid-svgrepo-com.svg">
+                <label>For Business</label>
+            </div>
+            <div class="top-icon">
+                <img style="height: 20px;" class="home" src="icons/business-svgrepo-com.svg">
+                <label>Post a job</label>
+            </div>
+        </div>
+    </header>
